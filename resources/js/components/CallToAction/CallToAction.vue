d6db5116ac78ea42676d23f0cec06a739a2a95cd
crystel <crystel.ratsimbazafy@swanforlife.com>
2024-12-14 18:28:17 +0300

    add background for video presentation

--- resources/js/components/CallToAction/CallToAction.vue
+++ resources/js/components/CallToAction/CallToAction.vue
@@ -1,34 +1,39 @@
 <template>
-    <div class="pt-[41px] pl-[40px] pr-[49px]">
-        <h1 class="text-[26px] w-[331px] mb-[14px] leading-[32px] font-title font-bold">Test psychotechnique : 
-            <span class="block">
-                Récupérez <span class="underline underline-offset-8 decoration-primary-500">votre permis</span>
-            </span>
-        </h1>
-        <span class="font-sans font-normal text-[17px]">Une démarche simple & rapide</span>
-    </div>
-    <div class="pt-[35px] pl-[59px]">
-        <ul class="flex flex-col gap-1.5">
-            <SuccessAction v-for="value in listOfSuccess" :key="value" :value="value"></SuccessAction>
-        </ul>
-    </div>
-    <div class="pt-[35px] flex items-center justify-center">
-        <Button 
-            :text="callToActionBtn.text"
-            :textStyle="callToActionBtn.textStyle"
-            :textClass="callToActionBtn.textClass"
-            :btnClass="callToActionBtn.btnClass"
-            :iconClass="callToActionBtn.iconClass"
-            :iconStyle="callToActionBtn.iconStyle"
-            :showIcon="callToActionBtn.showIcon"
-        ></Button>
-    </div>
-    <div class="pt-[11px] flex items-center justify-center">
-        <p class="text-[12px] font-body"><i class="fa-regular fa-hand-pointer pr-[8px]"></i> Entrainement Gratuit inclus</p>
-    </div>
-    <div class="pt-[20px] flex items-center justify-center gap-[31px]">
-        <GoogleRating :rating="googleRatingData.rating" :review="googleRatingData.review"></GoogleRating>
-        <TrustPilotRating :rating="googleRatingData.rating" :review="googleRatingData.review"></TrustPilotRating>
+    <div class="flex flex-col items-center justify-center">
+        <div class="pt-[41px] pl-[40px] pr-[49px]">
+                <h1 class="text-[26px] w-[331px] mb-[14px] leading-[32px] font-title font-bold">Test psychotechnique : 
+                    <span class="block">
+                        Récupérez <span class="underline underline-offset-8 decoration-primary-500">votre permis</span>
+                    </span>
+                </h1>
+                <span class="font-sans font-normal text-[17px]">Une démarche simple & rapide</span>
+            </div>
+        <div class="pt-[35px] pl-[59px]">
+            <ul class="flex flex-col gap-1.5">
+                <SuccessAction v-for="value in listOfSuccess" :key="value" :value="value"></SuccessAction>
+            </ul>
+        </div>
+        <div class="pt-[35px]">
+            <Button 
+                :text="callToActionBtn.text"
+                :textStyle="callToActionBtn.textStyle"
+                :textClass="callToActionBtn.textClass"
+                :btnClass="callToActionBtn.btnClass"
+                :iconClass="callToActionBtn.iconClass"
+                :iconStyle="callToActionBtn.iconStyle"
+                :showIcon="callToActionBtn.showIcon"
+            ></Button>
+        </div>
+        <div class="pt-[11px]">
+            <p class="text-[12px] font-body"><i class="fa-regular fa-hand-pointer pr-[8px]"></i> Entrainement Gratuit inclus</p>
+        </div>
+        <div class="pt-[20px] gap-[31px] flex items-center justify-center">
+            <GoogleRating :rating="googleRatingData.rating" :review="googleRatingData.review"></GoogleRating>
+            <TrustPilotRating :rating="googleRatingData.rating" :review="googleRatingData.review"></TrustPilotRating>
+        </div>
+        <div class="pt-[34px]">
+            <VideoPresentation />
+        </div>
     </div>
 </template>
 
@@ -39,6 +44,7 @@
     import Button from '@components/UI/Button/Button.vue';
     import GoogleRating from '@components/Rating/GoogleRating.vue'
     import TrustPilotRating from '@components/Rating/TrustPilotRating.vue';
+    import VideoPresentation from '../VideoPresentation/VideoPresentation.vue';
     
     const listOfSuccess = ref(ListOfSucessData);
     const callToActionBtn = ref({
---
+++ resources/js/components/VideoPresentation/VideoPresentation.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="call-video"></div>
+    <iframe 
+            width="358" 
+            height="235" 
+            src="https://www.youtube.com/embed/zmjPmzJFEFA" 
+            title="YouTube video player" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen>
+        </iframe>
+</template>
+
+<style scoped>
+    .call-video {
+        background: #BF2A6B 0% 0% no-repeat padding-box;
+        transform: matrix(1, 0.03, -0.03, 1, 0, 0);
+        margin-left: 25px;
+        border-top-left-radius: 75%;
+        border-top-right-radius: 45%;
+        border-bottom-left-radius: 50%;
+        border-bottom-right-radius: 70%;
+        width: 100vw;
+        height: 386px;
+    }
+    iframe {
+        box-shadow: 7px 6px 13px #00000085;
+    }
+</style>
