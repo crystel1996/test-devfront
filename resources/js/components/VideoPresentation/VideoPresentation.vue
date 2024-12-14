2b19f43f87d306315e1789de1e28271273d990d2
crystel <crystel.ratsimbazafy@swanforlife.com>
2024-12-14 18:33:17 +0300

    add position absolute

--- resources/js/components/VideoPresentation/VideoPresentation.vue
+++ resources/js/components/VideoPresentation/VideoPresentation.vue
@@ -1,20 +1,22 @@
 <template>
-    <div class="call-video"></div>
-    <iframe 
-            width="358" 
-            height="235" 
-            src="https://www.youtube.com/embed/zmjPmzJFEFA" 
-            title="YouTube video player" 
-            frameborder="0" 
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-            allowfullscreen>
-        </iframe>
+    <div class="call-video">
+        <div class="iframe-content flex items-center justify-center">
+            <iframe 
+                width="358" 
+                height="235" 
+                src="https://www.youtube.com/embed/zmjPmzJFEFA" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+            </iframe>
+        </div>
+    </div>
 </template>
 
 <style scoped>
     .call-video {
         background: #BF2A6B 0% 0% no-repeat padding-box;
-        transform: matrix(1, 0.03, -0.03, 1, 0, 0);
         margin-left: 25px;
         border-top-left-radius: 75%;
         border-top-right-radius: 45%;
@@ -22,7 +24,14 @@
         border-bottom-right-radius: 70%;
         width: 100vw;
         height: 386px;
+        position: relative;
     }
+    .iframe-content {
+        position: absolute;
+        top: 54.2px;
+        left: 41px;
+        right: 31px;
+    } 
     iframe {
         box-shadow: 7px 6px 13px #00000085;
     }
