c289205fe8bcef9070af30f55c0a8bf7df202f61
crystel <crystel.ratsimbazafy@swanforlife.com>
2024-12-16 20:47:14 +0300

    add stepper component

--- resources/js/components/Step/Step.vue
+++ resources/js/components/Step/Step.vue
@@ -1,13 +1,27 @@
 <template>
     <div class="pt-[21px] pr-[32px] pl-[32px] bg-white">
-        <Title :title="title"></Title>
+        <div class="mb-[46px]">
+            <Title :title="title"></Title>
+        </div>
+        <Stepper 
+            v-for="(step, index) in listOfSteps" 
+            :key="step" 
+            :step="index + 1" 
+            :title="step.title" 
+            :description="step.description"
+            :numberStep="listOfSteps.length"
+        >
+        </Stepper>
     </div>
 </template>
 
 <script setup>
     import { ref } from 'vue';
     import Title from '@components/Title/Title.vue';
+    import Stepper from '@components/Stepper/Stepper.vue';
+    import ListOfStepper from './Data/ListOfStepper.json'
 
     const title = ref('Votre test psychotechnique en 3 étapes :');
+    const listOfSteps = ref(ListOfStepper);
 
 </script>
---
+++ resources/js/components/Stepper/Stepper.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="flex pb-[44px] relative">
+        <div class="w-[42px] h-[42px] bg-white rounded-full flex items-center justify-center shadow step">
+            <span class="text-primary-500 pt-[9px] pb-[9px] pl-[15px] pr-[15px] font-bold font-[24px]">{{ step }}</span>
+        </div>
+        <div class="ml-[18px]">
+            <p class="font-bold">{{ title }}<span class="font-normal">{{ description }}</span></p>
+        </div>
+        <div v-if="step < numberStep" class="h-[100%] border-l-2 border-primary-500 absolute top-[42px] left-[21px] transform -translate-x-1/2"></div>
+    </div>
+</template>
+
+<script setup>  
+    const { title, description, step, numberStep } = defineProps({
+        title: {
+            type: String,
+            defaultValue: ''
+        },
+        description: {
+            type: String,
+            defaultValue: ''
+        },
+        step: {
+            type: Number,
+            defaultValue: 0
+        },
+        numberStep: {
+            type: Number,
+            defaultValue: 0
+        }
+    });
+
+</script>
+
+<style scoped>
+    .shadow {
+        box-shadow: 0px 3px 6px #00000029;
+    }
+    .step {
+        position: relative;
+    }
+</style>
