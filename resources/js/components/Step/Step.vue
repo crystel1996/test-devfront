86938c96f1e8fb5728c3fb55d861c9f914285635
crystel <crystel.ratsimbazafy@swanforlife.com>
2024-12-16 20:52:53 +0300

    add call to cation btn

--- resources/js/components/Step/Step.vue
+++ resources/js/components/Step/Step.vue
@@ -12,6 +12,17 @@
             :numberStep="listOfSteps.length"
         >
         </Stepper>
+        <div class="flex items-center justify-center">
+            <Button 
+                :text="callToActionBtn.text"
+                :textStyle="callToActionBtn.textStyle"
+                :textClass="callToActionBtn.textClass"
+                :btnClass="callToActionBtn.btnClass"
+                :iconClass="callToActionBtn.iconClass"
+                :iconStyle="callToActionBtn.iconStyle"
+                :showIcon="callToActionBtn.showIcon"
+            ></Button>
+        </div>
     </div>
 </template>
 
@@ -19,9 +30,18 @@
     import { ref } from 'vue';
     import Title from '@components/Title/Title.vue';
     import Stepper from '@components/Stepper/Stepper.vue';
-    import ListOfStepper from './Data/ListOfStepper.json'
+    import ListOfStepper from './Data/ListOfStepper.json';
+    import Button from '@components/UI/Button/Button.vue';
 
     const title = ref('Votre test psychotechnique en 3 étapes :');
     const listOfSteps = ref(ListOfStepper);
+    const callToActionBtn = ref({
+        text: "Je réserve mon test psychotechnique",
+        textClass: "text-white text-[14px] font-title font-medium",
+        btnClass: "bg-primary-500",
+        iconClass:"fa-solid fa-angle-right",
+        iconStyle: { color: "white" },
+        showIcon: true
+    });
 
 </script>
