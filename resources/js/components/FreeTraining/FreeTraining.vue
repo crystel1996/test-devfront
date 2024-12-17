e8a232e79b1505979833e9233ea85c8324c9d8cf
crystel <crystel.ratsimbazafy@swanforlife.com>
2024-12-17 18:53:34 +0300

    add image for free training

--- resources/js/components/FreeTraining/FreeTraining.vue
+++ resources/js/components/FreeTraining/FreeTraining.vue
@@ -1,21 +1,26 @@
 <template>
-    <div class="bg-white pt-[60px]">
-        <div class="mb-[36px]">
-            <Title :title="freeTraining.title"></Title>
+    <div class="bg-white pt-[60px] lg:pl-[107px] lg:pt-[224px] lg:pr-[134px] lg:flex">
+        <div class="lg:mt-[89px] lg:flex lg:flex-col lg:items-start lg:justify-start lg:w-[80%]">
+            <div class="mb-[36px]">
+                <Title :title="freeTraining.title"></Title>
+            </div>
+            <div class="mb-[40px]">
+                <div class="text-textColor2-500 text-[16px] pl-[36px] pr-[36px] lg:pl-[0] lg:text-[23px]" v-for="(text, index) in freeTraining.contents" :key="index" v-html="text"></div>
+            </div>
+            <div class="flex items-center justify-center lg:items-start lg:justify-start">
+                <Button 
+                    :text="callToActionBtn.text"
+                    :textStyle="callToActionBtn.textStyle"
+                    :textClass="callToActionBtn.textClass"
+                    :btnClass="callToActionBtn.btnClass"
+                    :iconClass="callToActionBtn.iconClass"
+                    :iconStyle="callToActionBtn.iconStyle"
+                    :showIcon="callToActionBtn.showIcon"
+                ></Button>
+            </div>
         </div>
-        <div class="mb-[40px]">
-            <div class="text-textColor2-500 text-[16px] pl-[36px] pr-[36px]" v-for="(text, index) in freeTraining.contents" :key="index" v-html="text"></div>
-        </div>
-        <div class="flex items-center justify-center">
-            <Button 
-                :text="callToActionBtn.text"
-                :textStyle="callToActionBtn.textStyle"
-                :textClass="callToActionBtn.textClass"
-                :btnClass="callToActionBtn.btnClass"
-                :iconClass="callToActionBtn.iconClass"
-                :iconStyle="callToActionBtn.iconStyle"
-                :showIcon="callToActionBtn.showIcon"
-            ></Button>
+        <div class="flex items-center justify-center lg:h-[668px] mt-[86px]">
+            <img class="w-[356px] h-[355px] lg:w-[669px] lg:h-[668px] object-cover" src="https://picsum.photos/300/300" alt="Step image" />
         </div>
     </div>
 </template>
